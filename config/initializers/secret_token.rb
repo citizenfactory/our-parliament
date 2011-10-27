45af8dca8ce04b5d2c96662fcc26df677cda6ba7
Peter Meadows <website@apathyisboring.com>
2011-10-27 16:21:04 -0400

    Revert "text fixes to debates and committees"
    This reverts commit 413ddea8d10f364dd2f4b6f3c57603bc5003bf7b.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-OurParliament::Application.config.secret_token = '5d210eef3747884b5e2afe02911ad4500feafb6207859cc14bd5559d35a620d396075ac4ef521cf6425aa5e15d472fd55d00df5f2bb8d03403c4270dc42b9ea3'
