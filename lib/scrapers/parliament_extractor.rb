0f7dbec59e4d0d720bcfcff3ddbebbe7f383ecbb
Paul Guelpa <paul.guelpa@gmail.com>
2011-11-02 01:20:50 -0400

    Added the notion of fresh vs stale data, so it doesn't get re-downloaded all the time

--- lib/scrapers/parliament_extractor.rb
+++ lib/scrapers/parliament_extractor.rb
@@ -4,24 +4,17 @@ module Scrapers
     PATH = ""
     QUERY_STRING = ""
 
-    attr_reader :logger, :output_dir
+    attr_reader :logger, :output_dir, :stale_at
 
     def initialize(options = {})
       @logger = options[:logger] || Rails.logger
       @output_dir = options[:output_dir] || File.join(Rails.root, "tmp", "data")
+      @stale_at = options[:stale_at] || 7.days.ago
     end
 
     def run
       prepare_output_dir
-
-      Net::HTTP.start(HOST) do |http|
-        response = http.get(url)
-        if response.code == "200"
-          File.open(output_file, "w") { |f| f.write(response.body) }
-        else
-         logger.error "#{response.code} #{response.message}"
-        end
-      end
+      download_file if stale?
     end
 
     def output_file
@@ -32,10 +25,25 @@ module Scrapers
       raise NotImplementedError
     end
 
+    def stale?
+      File.exist?(output_file) ? (File.mtime(output_file) < stale_at) : true
+    end
+
     private
 
     def prepare_output_dir
       FileUtils.mkdir_p(output_dir) if !File.directory?(output_dir)
+    end
+
+    def download_file
+      Net::HTTP.start(HOST) do |http|
+        response = http.get(url)
+        if response.code == "200"
+          File.open(output_file, "w") { |f| f.write(response.body) }
+        else
+         logger.error "#{response.code} #{response.message}"
+        end
+      end
     end
   end
 end
--- test/unit/scrapers/parliament_extractor_test.rb
+++ test/unit/scrapers/parliament_extractor_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
-class Scrapers::ParliamentExtractorTest < ActiveSupport::TestCase
-  def setup
-    File.stubs(:directory?).returns(true)
-    FileUtils.stubs(:mkdir_p)
-  end
-
+class Scrapers::ParliamentExtractorConfigurationTest < ActiveSupport::TestCase
   def test_default_output_dir
     default_dir = File.join(Rails.root, "tmp", "data")
     assert_equal default_dir, Scrapers::ParliamentExtractor.new.output_dir
@@ -15,12 +10,64 @@ class Scrapers::ParliamentExtractorTest < ActiveSupport::TestCase
     assert_equal "foo", Scrapers::ParliamentExtractor.new(:output_dir => "foo").output_dir
   end
 
+  def test_default_stale_at
+    now = Time.current
+    Time.stubs(:current).returns(now)
+    assert_equal (now - 7.days), Scrapers::ParliamentExtractor.new.stale_at
+  end
+
+  def test_stale_file_by_time
+    File.stubs(:exist?).returns(true)
+    File.stubs(:mtime).returns(3.days.ago)
+
+    extractor = Scrapers::ParliamentExtractor.new(:stale_at => 1.day.ago)
+    extractor.stubs(:output_file).returns("")
+
+    assert_equal true, extractor.stale?
+  end
+
+  def test_fresh_file
+    File.stubs(:exist?).returns(true)
+    File.stubs(:mtime).returns(2.days.ago)
+
+    extractor = Scrapers::ParliamentExtractor.new
+    extractor.stubs(:output_file).returns("")
+
+    assert_equal false, extractor.stale?
+  end
+
+  def test_stale_file_by_non_existance
+    File.stubs(:exist?).returns(false)
+
+    extractor = Scrapers::ParliamentExtractor.new
+    extractor.stubs(:output_file).returns("")
+
+    assert_equal true, extractor.stale?
+  end
+end
+
+class Scrapers::ParliamentExtractorRunTest < ActiveSupport::TestCase
+  def setup
+    File.stubs(:exist?).returns(false)
+    File.stubs(:directory?).returns(true)
+    FileUtils.stubs(:mkdir_p)
+    Scrapers::ParliamentExtractor.any_instance.stubs(:output_file).returns("")
+  end
+
   def test_output_dir_creation_on_run
     Net::HTTP.stubs(:start)
 
     File.expects(:directory?).returns(false)
     FileUtils.expects(:mkdir_p).with("foo_dir")
     Scrapers::ParliamentExtractor.new(:output_dir => "foo_dir").run
+  end
+
+  def test_not_run_on_fresh_data
+    extractor = Scrapers::ParliamentExtractor.new
+    extractor.expects(:stale?).returns(false)
+    extractor.expects(:download_file).never
+
+    extractor.run
   end
 
   def test_http_connection_with_correct_params
@@ -47,7 +94,7 @@ class Scrapers::ParliamentExtractorTest < ActiveSupport::TestCase
   def test_should_save_a_file_with_the_response_data
     extractor = Scrapers::ParliamentExtractor.new
     extractor.stubs(:url).returns("this_should_be_overridden_by_a_subclass")
-    extractor.expects(:output_file).returns("this_should_be_overridden_by_a_sublcass")
+    extractor.expects(:output_file).at_least_once.returns("this_should_be_overridden_by_a_sublcass")
 
     stub_response = stub("HTTP Response", :code => "200", :body => :foo_response)
     stub_http = stub("http connection", :get => stub_response)
@@ -74,5 +121,4 @@ class Scrapers::ParliamentExtractorTest < ActiveSupport::TestCase
 
     extractor.run
   end
-
 end
