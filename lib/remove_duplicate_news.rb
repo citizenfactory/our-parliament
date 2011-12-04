b9c1a39e5c0204699f556a53bc9a118eef23930f
Paul Guelpa <paul.guelpa@gmail.com>
2011-12-04 17:18:00 -0500

    Trying to break down the news cleanup to a by mp/senator id because it's failing to do it all at once

--- lib/remove_duplicate_news.rb
+++ lib/remove_duplicate_news.rb
@@ -1,19 +1,27 @@
 class RemoveDuplicateNews
   class << self
     def for_mps
-      query = "DELETE FROM mps_news_articles a WHERE ctid NOT IN (
-        SELECT MAX(b.ctid) FROM mps_news_articles b
-        GROUP BY b.mp_id, b.news_article_id
-      )"
-      ActiveRecord::Base.connection.execute(query)
+      Mp.all.each do |mp|
+        puts "Cleaning MP: #{mp.name}"
+        query = "DELETE FROM mps_news_articles a WHERE a.mp_id = #{mp.id} AND a.ctid NOT IN (
+          SELECT MAX(b.ctid) FROM mps_news_articles b
+          WHERE b.mp_id = #{mp.id}
+          GROUP BY b.mp_id, b.news_article_id
+        )"
+        ActiveRecord::Base.connection.execute(query)
+      end
     end
 
     def for_senators
-      query = "DELETE FROM senators_news_articles a WHERE ctid NOT IN (
-        SELECT MAX(b.ctid) FROM senators_news_articles b
-        GROUP BY b.senator_id, b.news_article_id
-      )"
-      ActiveRecord::Base.connection.execute(query)
+      Senator.all.each do |senator|
+        puts "Cleaning Senator: #{senator.name}"
+        query = "DELETE FROM senators_news_articles a WHERE a.senator_id = #{senator.id} AND a.ctid NOT IN (
+          SELECT MAX(b.ctid) FROM senators_news_articles b
+          WHERE b.senator_id = #{senator.id}
+          GROUP BY b.senator_id, b.news_article_id
+        )"
+        ActiveRecord::Base.connection.execute(query)
+      end
     end
   end
 end
